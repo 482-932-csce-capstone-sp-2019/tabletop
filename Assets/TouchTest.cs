d5055d3817d24c524d623ce8981111023ec6fca4
ryanmeyers <ryanmeyers@tamu.edu>
2019-01-31 10:14:14 -0600

    Blank touch test initial commit

---
+++ Assets/TouchTest.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchTest : MonoBehaviour 
+{
+    void Update () 
+    {
+        Touch[] myTouches = Input.touches;
+        print(myTouches.Length + " touches detected.");
+    }
+}
