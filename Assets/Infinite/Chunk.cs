47676b4cfe65d54c317342c504f368b4d2322fe6
ryanmeyers <ryanmeyers@tamu.edu>
2019-02-26 10:18:44 -0600

    camera movement + start of map optimization

---
+++ Assets/Infinite/Chunk.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chunk : MonoBehaviour
+{
+    public static int size = 25;
+    Tile[,] tiles;
+
+    public Sprite full;
+    public Sprite empty;
+    public Sprite grass;
+
+    void Awake()
+    {
+        tiles = new Tile[size, size];
+
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                tiles[i,j] = new Tile(i + (int)transform.position.x, j + (int)transform.position.y, 0);
+                GameObject tileGO = new GameObject("Tile (" + (i + (int)transform.position.x) + "," + (j + (int)transform.position.y) + ")");
+                tileGO.transform.position = new Vector3(tiles[i,j].x, tiles[i,j].y, tiles[i,j].z);
+                tileGO.transform.SetParent(this.transform, true);
+                SpriteRenderer spriteRenderer = tileGO.AddComponent<SpriteRenderer>();
+                spriteRenderer.sprite = full;
+            }
+        }
+    }
+}
---
+++ Assets/Infinite/Tile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile
+{
+    public enum Type { empty, full }
+    Type type;
+    public int x { get; private set; }
+    public int y { get; private set; }
+    public int z { get; private set; }
+
+    public Tile(int x, int y, int z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+
+        type = Type.full;
+    }
+}
