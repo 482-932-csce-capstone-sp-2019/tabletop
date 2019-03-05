690e1ec370af4625469b05293b77e8287aaa9ce9
michaelplaster <michaelplaster@tamu.edu>
2019-03-05 09:54:31 -0600

    Add files via upload
    Scripts for User Interfaces

---
+++ Assets/Scripts/UIManagerScript.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManagerScript : MonoBehaviour
+{
+	public void materialSelect() 
+	{
+    	
+	}
+
+	public void ShallowWaterSelect() 
+	{
+    	
+	}
+
+	public void WaterSelect() 
+	{
+    	
+	}
+
+	public void WoodSelect() 
+	{
+    	
+	}
+
+	public void GrassSelect() 
+	{
+    	
+	}
+
+	public void LavaSelect() 
+	{
+    	
+	}
+
+	public void SnowIceSelect() 
+	{
+    	
+	}
+
+	public void StoneSelect() 
+	{
+    	
+	}
+
+	public void EmptyVoidSelect() 
+	{
+    	
+	}
+
+	public void PlayerCharacterSelect() 
+	{
+    	
+	}
+
+	public void NPCSelect() 
+	{
+    	
+	}
+
+	public void MonsterSelect() 
+	{
+    	
+	}
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.LoadScene("UI Material");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
