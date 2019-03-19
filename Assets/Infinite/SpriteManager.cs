4d88637fc051dc9817ff87c7a9a3970d2092b4d2
ryanmeyers <ryanmeyers@tamu.edu>
2019-03-19 08:24:52 -0500

    Map stuff

---
+++ Assets/Infinite/SpriteManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteManager : MonoBehaviour
+{
+    public static SpriteManager instance;
+    Dictionary<string, Sprite> tileSprites;
+
+    void Awake()
+    {
+        instance = this;
+        tileSprites = new Dictionary<string, Sprite>();
+        loadSprites();    
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Sprite getSprite(Tile tile)
+    {
+        if(tileSprites.ContainsKey(tile.type.ToString()))
+        {
+            return tileSprites[tile.type.ToString()];
+        }
+        Debug.LogError("Unrecognized tile type: " + tile.type.ToString());
+        return null;
+    }
+
+    void loadSprites()
+    {
+       Sprite [] sprites = Resources.LoadAll<Sprite>("tilesheet");
+       foreach(Sprite s in sprites)
+       {
+           tileSprites.Add(s.name, s);
+       }
+    }
+}
---
+++ Assets/Infinite/unit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class unit : MonoBehaviour
+{
+    public int tileX;
+	public int tileY;
+    public Color selected = Color.white;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Renderer[] models = GetComponentsInChildren<Renderer>();
+		foreach ( Renderer r in models)
+			r.material.color = selected;
+    }
+}
