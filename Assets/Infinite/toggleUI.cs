f989d7774d139d4cf556c11e49817f9660fe31e8
ryanmeyers <ryanmeyers@tamu.edu>
2019-03-19 08:59:17 -0500

    UI toggle fix

---
+++ Assets/Infinite/toggleUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toggleUI : MonoBehaviour
+{
+    public GameObject UI;
+    bool status;
+    // Start is called before the first frame update
+    void Start()
+    {
+        status = false;
+    }
+
+    public void toggle()
+    {
+        if(status)
+        {
+            status = false;
+            UI.gameObject.SetActive(false);
+        }
+        else
+        {
+            status = true;
+            UI.gameObject.SetActive(true);
+        }
+    }
+}
