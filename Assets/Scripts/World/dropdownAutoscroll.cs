298361ea288e3492abea0a9569b60a244aee01c9
James Vanderburg <james@vanderburg.org>
2019-04-25 14:02:58 -0500

    Fix autoscroll errors

--- Assets/Scripts/World/dropdownAutoscroll.cs
+++ Assets/Scripts/World/dropdownAutoscroll.cs
@@ -7,15 +7,15 @@ using UnityEngine.EventSystems;
 using System.Collections.Generic;
 
 public class dropdownAutoscroll : MonoBehaviour {
- 
+
     [Tooltip("Assign to the dropdown that should automatically scroll according to the currently selected item.")]
     public Dropdown dropdown;
- 
+
     // Use this for initialization
     void Start ()
     {
     }
-   
+
     // Update is called once per frame
     void Update () {
         if (EventSystem.current.currentSelectedGameObject == dropdown.gameObject)
@@ -23,6 +23,7 @@ public class dropdownAutoscroll : MonoBehaviour {
             if (Input.GetButtonUp("Vertical"))
             {
                 Transform dropdownListTransform = dropdown.gameObject.transform.Find("Dropdown List");
+                //Debug.Log(dropdown.gameObject.transform.childCount);
                 if (dropdownListTransform == null)
                 {
                     // Show the dropdown when the user hits the arrow keys if the dropdown is not already showing
@@ -31,7 +32,7 @@ public class dropdownAutoscroll : MonoBehaviour {
             }
         }
         else
-        {  
+        {
             PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
             eventDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
             List<RaycastResult> results = new List<RaycastResult>();
@@ -44,46 +45,49 @@ public class dropdownAutoscroll : MonoBehaviour {
                     return;
                 }
             }
-           
+
             // Autoscroll list as the selected object is changed from the arrow keys
-            if (EventSystem.current.currentSelectedGameObject.transform.IsChildOf(dropdown.gameObject.transform))
+            if (EventSystem.current.currentSelectedGameObject != null)
             {
-                if (EventSystem.current.currentSelectedGameObject.name.StartsWith("Item "))
+                if (EventSystem.current.currentSelectedGameObject.transform.IsChildOf(dropdown.gameObject.transform))
                 {
-                    // Skip disabled items
-                    Transform parent = EventSystem.current.currentSelectedGameObject.transform.parent;
-                    int activeChildren = 0;
-                    int totalChildren = parent.childCount;
-                    for (int childIndex = 0; childIndex < totalChildren; childIndex++)
+                    if (EventSystem.current.currentSelectedGameObject.name.StartsWith("Item "))
                     {
-                        if (parent.GetChild(childIndex).gameObject.activeInHierarchy)
+                        // Skip disabled items
+                        Transform parent = EventSystem.current.currentSelectedGameObject.transform.parent;
+                        int activeChildren = 0;
+                        int totalChildren = parent.childCount;
+                        for (int childIndex = 0; childIndex < totalChildren; childIndex++)
                         {
-                            activeChildren++;
+                            if (parent.GetChild(childIndex).gameObject.activeInHierarchy)
+                            {
+                                activeChildren++;
+                            }
                         }
-                    }
-                    int myActiveIndex = 0;
-                    for (int childIndex = 0; childIndex < totalChildren; childIndex++)
-                    {
-                        if (parent.GetChild(childIndex).gameObject == EventSystem.current.currentSelectedGameObject)
+                        int myActiveIndex = 0;
+                        for (int childIndex = 0; childIndex < totalChildren; childIndex++)
                         {
-                            break;
+                            if (parent.GetChild(childIndex).gameObject == EventSystem.current.currentSelectedGameObject)
+                            {
+                                break;
+                            }
+                            else if (parent.GetChild(childIndex).gameObject.activeInHierarchy)
+                            {
+                                myActiveIndex++;
+                            }
                         }
-                        else if (parent.GetChild(childIndex).gameObject.activeInHierarchy)
+
+                        if (activeChildren > 1)
                         {
-                            myActiveIndex++;
-                        }
-                    }
- 
-                    if (activeChildren > 1)
-                    {
-                        GameObject scrollbarGameObject = GameObject.Find ("Scrollbar");
-                        if (scrollbarGameObject != null && scrollbarGameObject.activeInHierarchy)
-                        {
-                            Scrollbar scrollbar = scrollbarGameObject.GetComponent<Scrollbar>();
-                            if (scrollbar.direction == Scrollbar.Direction.TopToBottom)
-                                scrollbar.value = (float) myActiveIndex / (float) (activeChildren-1);
-                            else
-                                scrollbar.value = 1.0f - (float) myActiveIndex / (float) (activeChildren-1);
+                            GameObject scrollbarGameObject = GameObject.Find("Scrollbar");
+                            if (scrollbarGameObject != null && scrollbarGameObject.activeInHierarchy)
+                            {
+                                Scrollbar scrollbar = scrollbarGameObject.GetComponent<Scrollbar>();
+                                if (scrollbar.direction == Scrollbar.Direction.TopToBottom)
+                                    scrollbar.value = (float)myActiveIndex / (float)(activeChildren - 1);
+                                else
+                                    scrollbar.value = 1.0f - (float)myActiveIndex / (float)(activeChildren - 1);
+                            }
                         }
                     }
                 }
